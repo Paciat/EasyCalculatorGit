5de06677d99c08e35614d7e9681f6a393d10bdf7
Paci <paciat@yahoo.de>
2015-11-09 20:41:51 +0100

    new commit

---
+++ src/easycalculatorGit/Darstellbar.java
@@ -0,0 +1,30 @@
+package easycalculator;
+
+/**
+ * Interface Darstellbar - legt die Methoden fest, die eine Ausgabeeinheit,
+ * bspw. Konsole beherrschen muss.
+ *
+ * @author Patricia Füruter
+ */
+public interface Darstellbar {
+
+    /**
+     * Methode gibRechenArt übernimmt den Name (Funktionsnamen) des Objekts und
+     * kann damit bspw. einen String auf der Konsole ausgeben.
+     *
+     * @param name
+     */
+    public void gibRechenArt(String name);
+
+    /**
+     * Methode ausgebenErgebnis übernimmtdie einzelenen Rechnungsbestandteile
+     * kann damit bspw. einen String auf der Konsole ausgeben..
+     *
+     * @param zahl1
+     * @param zahl2
+     * @param rechenzeichen
+     * @param ergebnis
+     */
+    public void ausgebenErgebnis(double zahl1, double zahl2, char rechenzeichen, double ergebnis);
+
+}
